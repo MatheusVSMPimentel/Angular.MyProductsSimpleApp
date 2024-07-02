82c9e9559e5b6f1c4e2d4b2c84a2c5813ed03842
Matheus Pimentel <matheusvsm.pimentel@hotmail.com>
2024-07-02 07:19:17 -0300

    feat: config providers, config app imports, bootstrap and font awesome import, insert navigation and router tag param into app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './navigation/home/home.component';
+import { MenuComponent } from './navigation/menu/menu.component';
+import { FooterComponent } from './navigation/footer/footer.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  imports: [RouterOutlet, HomeComponent, MenuComponent, FooterComponent],
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
+  constructor() {
+    registerLocaleData(localePt)
+    
+  }
   title = 'Angular.MyProductsApp';
 }
